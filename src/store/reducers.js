3abae93b856098074e56d13c71f0a816e12ec042
Taylor Radtke <taylorradtke@gmail.com>
2020-02-07 10:19:34 -0500

    added tracks to root reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,12 +4,14 @@ import { connectRouter } from "connected-react-router";
 //* Reducers
 import browser from "./browser/reducer";
 import tracklists from "./tracklists/tracklists-reducer";
+import tracks from "./tracks/reducer";
 
 const createRootReducer = history =>
   combineReducers({
     router: connectRouter(history),
     browser,
     tracklists,
+    tracks,
   });
 
 export default createRootReducer;
