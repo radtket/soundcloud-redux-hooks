c69adf5766df961bed49d390eb683fff46fe5d89
Taylor Radtke <taylorradtke@gmail.com>
2020-02-26 14:47:18 -0500

    hide session options in nav

--- src/views/components/Navbar.js
+++ src/views/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink, Link } from "react-router-dom";
 import StyledNavbar from "../styles/Navbar";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 import UserAccountButton from "./UserAccountButton";
+import { getSession } from "../../store/session/selectors";
 
 const Navbar = () => {
+  const { id } = useSelector(getSession);
   return (
     <StyledNavbar>
       <div className="nav-container">
@@ -19,16 +22,16 @@ const Navbar = () => {
           <NavLink className="nav-item" to="/genres">
             Genres
           </NavLink>
-          <NavLink className="nav-item" to="/library">
-            Library
-          </NavLink>
         </nav>
 
-        <nav className="nav-secondary">
-          <NavLink to="/me/stream">Stream</NavLink>
-          <NavLink to="/me/likes">Likes</NavLink>
-          <NavLink to="/me/followings">Following</NavLink>
-        </nav>
+        {id && (
+          <nav className="nav-secondary">
+            <NavLink to="/me/stream">Stream</NavLink>
+            <NavLink to="/me/likes">Likes</NavLink>
+            <NavLink to="/me/followings">Following</NavLink>
+          </nav>
+        )}
+
         <nav className="nav-search">
           <SearchBar />
           <UserAccountButton />
